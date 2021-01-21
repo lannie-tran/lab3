f1c6ccf4614b312eab9aa7c5b7d67bdbcd1df8a1
Lannie Tran <lht009@ucsd.edu>
2021-01-21 10:38:58 -0800

    defined projectClick function and controlled content visibility

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,11 +8,29 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript is working :-)");
+		$(".jumbotron p") .toggleClass("active");
+		$("#testjs").text("Please wait...");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+function projectClick(e) {
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if(description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else{
+		description.html("<p>Stop clicking on me! You just did it at" + (new Date()) + "</p>");
+		$(description).fadeToggle()
+	}
+}
+
+$("a.thumbnail").click(projectClick);
 }
